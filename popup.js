19da22ae3d0c51a6f5a017ec64d66e20def96724
Elisabeth Seite <elisabeth.seite@gmail.com>
2017-09-21 21:38:50 -0400

    Fetching data from server API working

--- popup.js
+++ popup.js
@@ -1,16 +1,34 @@
-chrome.contextMenus.create({
-  "id": '47',
-  "title": "Partage this page",
-  "contexts": ["all"],
-})
-
-chrome.contextMenus.onClicked.addListener(function(info, tab) {
-  var url = tab.url;
-  if (info.menuItemId == "47") {
-      alert(url);
-  }
-});
 
 chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
-  // Use the token.
+  console.log('token', token)
 });
+
+// function click(){
+//   $('#div1').click(function(){
+//     console.log('i was clicked!')
+//     $.ajax({url: "http://localhost:8080/api/messages", success: function(result){
+//       const val = result[0].content;
+//       console.log('ajax call was successful', val)
+//       $('#div1').html(val)
+//     }}
+//   )})
+// }
+
+chrome.identity.getProfileUserInfo(function(cb) {
+  console.log('identity', cb)
+})
+
+function init(){
+
+  $('#div1').on('click',
+    console.log('i was clicked!')
+    // $.ajax({url: "http://localhost:8080/api/messages", success: function(result){
+    //   const val = result[0].content;
+    //   console.log('ajax call was successful', val)
+    //   $('#div1').html(val)
+    // }}
+
+
+)}
+
+document.addEventListener('DOMContentLoaded', init);
